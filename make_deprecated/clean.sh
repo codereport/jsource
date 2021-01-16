f9ed776fedf422527b4fceffeca3ad4703127329
Conor Hoekstra <codereport@outlook.com>
2021-01-15 19:34:48 -0500

    Remove make_deprecated

--- make_deprecated/clean.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# rm all *.o for clean builds - makefile dependencies are not set 
-find $jbld/jout -name "*.o" -type f -delete
-find $jgit/jsrc -name "*.o" -type f -delete
-find $jgit/dllsrc -name "*.o" -type f -delete
-
-
-
