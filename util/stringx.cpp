d57cdd85108254b884b1f90c29a37de824244dad
Ahmad Beirkdar <ahmadb@protonmail.com>
2021-01-25 09:38:05 -0500

    string helper functions added

---
+++ util/stringx.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Ahmad Beirkdar on 2021-01-25.
+//
+
+#include "stringx.h"
+
+
+namespace util{
+    void removeAllOccurrences(std::string& str, const std::string& sub_str){
+        auto n = str.find(sub_str);
+        while(n != std::string::npos){
+            str.replace(n,sub_str.size(),"");
+            n = str.find(sub_str);
+        }
+    }
+}
---
+++ util/stringx.h
@@ -0,0 +1,12 @@
+//
+// Created by Ahmad Beirkdar on 2021-01-25.
+//
+
+#ifndef J_STRINGX_H
+#define J_STRINGX_H
+#include <string>
+
+namespace util{
+    void removeAllOccurrences(std::string& str, const std::string& sub_str);
+}
+#endif //J_STRING_H
