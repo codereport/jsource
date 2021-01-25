7eae4754c870501f9f73ecf4299551ac7a9cb002
Ahmad Beirkdar <ahmadb@protonmail.com>
2021-01-25 16:15:53 -0500

    used std::string::erase instead of replace, and start search from the position of the last occurence

--- util/stringx.cpp
+++ util/stringx.cpp
@@ -6,8 +6,8 @@ namespace util{
     void removeAllOccurrences(std::string& str, const std::string& sub_str){
         auto n = str.find(sub_str);
         while(n != std::string::npos){
-            str.replace(n,sub_str.size(),"");
-            n = str.find(sub_str);
+            str.erase(n,sub_str.size());
+            n = str.find(sub_str,n);
         }
     }
 }
