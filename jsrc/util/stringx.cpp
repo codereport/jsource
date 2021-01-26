f6b4f4e8635f1b22a9fc5b962f4c83fe97c5a641
Ahmad Beirkdar <ahmadb@protonmail.com>
2021-01-26 10:34:48 -0500

    util::remove_all_occurrences sub_str type changed to string_view

--- jsrc/util/stringx.cpp
+++ jsrc/util/stringx.cpp
@@ -3,7 +3,7 @@
 
 
 namespace util{
-    auto remove_all_occurrences(std::string &str, std::string const &sub_str) -> void {
+    auto remove_all_occurrences(std::string &str, std::string_view const &sub_str) -> void {
         size_t n = 0;
         while((n = str.find(sub_str, n)) != std::string::npos)
             str.erase(n,sub_str.size());
