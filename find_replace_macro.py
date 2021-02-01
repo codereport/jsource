e180ada6bad7746a0977d01190219e2565547064
Sebanisu <peeceseven@gmail.com>
2021-02-01 16:34:12 -0500

    Create find_replace_macro.py
    Goal is to read all the .h and .c files and remove some macros.

---
+++ find_replace_macro.py
@@ -0,0 +1,55 @@
+import os
+import re
+
+rootdir = r"jsrc"
+
+
+def walk_path_cpp():
+    for root, subdirs, files in os.walk(rootdir):
+        for file in files:
+            ext = os.path.splitext(file)[1]
+            if ext != ".c" and ext != ".h":
+                continue
+            yield os.path.join(root, file)
+    pass
+
+
+def walk_matches():
+    for path in walk_path_cpp():
+        with open(path, 'r') as f:
+            matches = re.findall(r'#define\s+([\w\d]+)\(([^,]+),([^),]+)\)\s+(jt[\d\w]+)\(jt,\(\2\),\(\3\)\)', f.read())
+            if len(matches) == 0:
+                continue
+            for name1, v1, v2, name2 in matches:
+                yield r'(^|[ \t]+)' + name1 + r'\(([^,]+?),([^)]+?)\)', name2 + r'(jt,(\2),(\3))'
+    pass
+
+
+def find_replaced_data():
+    for regular_expression, replace_pattern in walk_matches():
+        # print(regular_expression)
+        # print(replace_pattern)
+        for path in walk_path_cpp():
+            # print(path)
+
+            with open(path, 'r') as f:
+                data = f.read()
+                matches = re.search(regular_expression, data)
+                if matches is None:
+                    continue
+                print(matches)
+                yield path, re.sub(regular_expression, replace_pattern, data)
+    pass
+
+
+def main():
+    i = 0
+    for path, new_data in find_replaced_data():
+        with open(path, 'w') as f:
+            f.write(new_data)
+            i += 0
+    print(str(i) + " replacements")
+    pass
+
+
+main()
