ab95df6b1da253c13b67f17a332636b2ccc563a6
Sebanisu <peeceseven@gmail.com>
2021-02-02 18:56:13 -0500

    Update find_replace_macro.py
    moving this variable into the global as when the second part doesn't replace anything the new name didn't get passed to git

--- find_replace_macro.py
+++ find_replace_macro.py
@@ -2,6 +2,7 @@ import os
 import re
 
 rootdir = r"jsrc"
+old_name = ""  # only doing one name at a time. Maybe not work.
 
 
 def walk_path_cpp():
@@ -15,6 +16,7 @@ def walk_path_cpp():
 
 
 def walk_matches():
+    global old_name
     for path in walk_path_cpp():
         data = None
         with open(path, 'r') as f:
@@ -26,27 +28,28 @@ def walk_matches():
             if len(matches) == 0:
                 continue
 
-            for full_str, name1, v1, v2, name2 in matches:
-                if name1 in ["bindd",
-                             "binzz",
-                             "dgcd",
-                             "dlcm",
-                             "equ"]:
+            for full_str, old_name, v1, v2, new_name in matches:
+                if old_name in ["bindd",
+                                "binzz",
+                                "dgcd",
+                                "dlcm",
+                                "equ"]:
                     continue
                 print("remove " + full_str)
                 data = data.replace(full_str, "")
                 with open(path, 'w') as fw:
                     fw.write(data)
-                yield re.compile(
-                    r'(^|[ \t]+|[^\d\w_])' + name1 + r'\((?=([^,]+?),([^)]+?)\))'), r'\1' + name2 + r'(jt,', name1, name2
+                yield re.compile(r'(^|[ \t]+|[^\d\w_])' + old_name + r'\((?=([^,]+?),([^)]+?)\))'), \
+                      r'\1' + new_name + r'(jt,', old_name, new_name
                 return
 
     pass
 
 
 def find_replaced_data():
+    global old_name
     unescaped = re.compile(r'\|')
-    for regular_expression, replace_pattern, oldname, newname in walk_matches():
+    for regular_expression, replace_pattern, old_name, new_name in walk_matches():
         # print(regular_expression)
         # print(replace_pattern)
         for path in walk_path_cpp():
@@ -61,27 +64,26 @@ def find_replaced_data():
                 with open("removal_log.md", 'a') as log:
                     escaped = '\\|'
                     log.write('\n|`' + path + '`')
-                    log.write('|' + oldname)
-                    log.write('|' + newname)
+                    log.write('|' + old_name)
+                    log.write('|' + new_name)
                     log.write('|`' + unescaped.sub(escaped, regular_expression.pattern) + '`')
                     log.write('|`' + unescaped.sub(escaped, replace_pattern) + '`|')
                 # print(matches)
-                yield path, regular_expression.sub(replace_pattern, data), oldname
+                yield path, regular_expression.sub(replace_pattern, data), old_name
     pass
 
 
 def main():
+    global old_name
     i = 0
     matches = [i for i in find_replaced_data()]
-    oldname = ""  # only doing one name at a time. Maybe not work.
-    for path, new_data, oldname in matches:
+
+    for path, new_data, old_name in matches:
         with open(path, 'w') as f:
             f.write(new_data)
             i += 1
-
-    # print(str(len(matches)+1) + " file changes")
     print()
-    os.system('git commit -m "remove #define ' + oldname + '" -a')
+    os.system('git commit -m "remove #define ' + old_name + '" -a')
     os.system('git push')
     pass
 
