34a5e61942f1a01cd403f47233bdc6f64ea6273e
Sebanisu <peeceseven@gmail.com>
2021-02-02 13:24:24 -0500

    exclude binzz
    There were some uncommon references to this macro. Where the name is passed to another macro.

--- find_replace_macro.py
+++ find_replace_macro.py
@@ -27,7 +27,8 @@ def walk_matches():
                 continue
 
             for full_str, name1, v1, v2, name2 in matches:
-                if name1 in ["bindd"]:
+                if name1 in ["bindd",
+                             "binzz"]:
                     continue
                 print("remove " + full_str)
                 data = data.replace(full_str, "")
