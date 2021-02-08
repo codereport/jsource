ac4653139e46be42bdb13e9a658c900971737d4d
Herman Berget <32880010+hermabe@users.noreply.github.com>
2021-02-08 21:28:32 +0100

    Remove USE_TERMIOS and USE_WINCONSOLE

--- jsrc/linenoise.c
+++ jsrc/linenoise.c
@@ -120,7 +120,6 @@
 #include <termios.h>
 #include <sys/ioctl.h>
 #include <poll.h>
-#define USE_TERMIOS
 #define HAVE_UNISTD_H
 
 #ifdef HAVE_UNISTD_H
@@ -866,21 +865,7 @@ struct current {
     const char *prompt;
     stringbuf *capture; /* capture buffer, or NULL for none. Always null terminated */
     stringbuf *output;  /* used only during refreshLine() - output accumulator */
-#if defined(USE_TERMIOS)
-    int fd;     /* Terminal fd */
-#elif defined(USE_WINCONSOLE)
-    HANDLE outh; /* Console output handle */
-    HANDLE inh; /* Console input handle */
-    int rows;   /* Screen rows */
-    int x;      /* Current column during output */
-    int y;      /* Current row */
-#ifdef USE_UTF8
-    #define UBUF_MAX_CHARS 132
-    WORD ubuf[UBUF_MAX_CHARS + 1];  /* Accumulates utf16 output - one extra for final surrogate pairs */
-    int ubuflen;      /* length used in ubuf */
-    int ubufcols;     /* how many columns are represented by the chars in ubuf? */
-#endif
-#endif
+    int fd;     /* Terminal fd */   
 };
 
 static int fd_read(struct current *current);
@@ -1026,387 +1011,6 @@ static void DRL_STR(const char *str)
 #define DRL_STR(str)
 #endif
 
-#if defined(USE_WINCONSOLE)
-// #include "linenoise-win32.c"
-
-/* this code is not standalone
- * it is included into linenoise.c
- * for windows.
- * It is deliberately kept separate so that
- * applications that have no need for windows
- * support can omit this
- */
-static DWORD orig_consolemode = 0;
-
-static int flushOutput(struct current *current);
-static void outputNewline(struct current *current);
-
-static void refreshStart(struct current *current)
-{
-}
-
-static void refreshEnd(struct current *current)
-{
-}
-
-static void refreshStartChars(struct current *current)
-{
-    assert(current->output == NULL);
-    /* We accumulate all output here */
-    current->output = sb_alloc();
-#ifdef USE_UTF8
-    current->ubuflen = 0;
-#endif
-}
-
-static void refreshNewline(struct current *current)
-{
-    DRL("<nl>");
-    outputNewline(current);
-}
-
-static void refreshEndChars(struct current *current)
-{
-    assert(current->output);
-    flushOutput(current);
-    sb_free(current->output);
-    current->output = NULL;
-}
-
-static int enableRawMode(struct current *current) {
-    DWORD n;
-    INPUT_RECORD irec;
-
-    current->outh = GetStdHandle(STD_OUTPUT_HANDLE);
-    current->inh = GetStdHandle(STD_INPUT_HANDLE);
-
-    if (!PeekConsoleInput(current->inh, &irec, 1, &n)) {
-        return -1;
-    }
-    if (getWindowSize(current) != 0) {
-        return -1;
-    }
-    if (GetConsoleMode(current->inh, &orig_consolemode)) {
-        SetConsoleMode(current->inh, ENABLE_PROCESSED_INPUT);
-    }
-#ifdef USE_UTF8
-    /* XXX is this the right thing to do? */
-    SetConsoleCP(65001);
-#endif
-    return 0;
-}
-
-static void disableRawMode(struct current *current)
-{
-    SetConsoleMode(current->inh, orig_consolemode);
-}
-
-void linenoiseClearScreen(void)
-{
-    /* XXX: This is ugly. Should just have the caller pass a handle */
-    struct current current;
-
-    current.outh = GetStdHandle(STD_OUTPUT_HANDLE);
-
-    if (getWindowSize(&current) == 0) {
-        COORD topleft = { 0, 0 };
-        DWORD n;
-
-        FillConsoleOutputCharacter(current.outh, ' ',
-            current.cols * current.rows, topleft, &n);
-        FillConsoleOutputAttribute(current.outh,
-            FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN,
-            current.cols * current.rows, topleft, &n);
-        SetConsoleCursorPosition(current.outh, topleft);
-    }
-}
-
-static void cursorToLeft(struct current *current)
-{
-    COORD pos;
-    DWORD n;
-
-    pos.X = 0;
-    pos.Y = (SHORT)current->y;
-
-    FillConsoleOutputAttribute(current->outh,
-        FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN, current->cols, pos, &n);
-    current->x = 0;
-}
-
-#ifdef USE_UTF8
-static void flush_ubuf(struct current *current)
-{
-    COORD pos;
-    DWORD nwritten;
-    pos.Y = (SHORT)current->y;
-    pos.X = (SHORT)current->x;
-    SetConsoleCursorPosition(current->outh, pos);
-    WriteConsoleW(current->outh, current->ubuf, current->ubuflen, &nwritten, 0);
-    current->x += current->ubufcols;
-    current->ubuflen = 0;
-    current->ubufcols = 0;
-}
-
-static void add_ubuf(struct current *current, int ch)
-{
-    /* This code originally by: Author: Mark E. Davis, 1994. */
-    static const int halfShift  = 10; /* used for shifting by 10 bits */
-
-    static const DWORD halfBase = 0x0010000UL;
-    static const DWORD halfMask = 0x3FFUL;
-
-    #define UNI_SUR_HIGH_START  0xD800
-    #define UNI_SUR_HIGH_END    0xDBFF
-    #define UNI_SUR_LOW_START   0xDC00
-    #define UNI_SUR_LOW_END     0xDFFF
-
-    #define UNI_MAX_BMP 0x0000FFFF
-
-    if (ch > UNI_MAX_BMP) {
-        /* convert from unicode to utf16 surrogate pairs
-         * There is always space for one extra word in ubuf
-         */
-        ch -= halfBase;
-        current->ubuf[current->ubuflen++] = (WORD)((ch >> halfShift) + UNI_SUR_HIGH_START);
-        current->ubuf[current->ubuflen++] = (WORD)((ch & halfMask) + UNI_SUR_LOW_START);
-    }
-    else {
-        current->ubuf[current->ubuflen++] = ch;
-    }
-    current->ubufcols += utf8_width(ch);
-    if (current->ubuflen >= UBUF_MAX_CHARS) {
-        flush_ubuf(current);
-    }
-}
-#endif
-
-static int flushOutput(struct current *current)
-{
-    const char *pt = sb_str(current->output);
-    int len = sb_len(current->output);
-
-#ifdef USE_UTF8
-    /* convert utf8 in current->output into utf16 in current->ubuf
-     */
-    while (len) {
-        int ch;
-        int n = utf8_tounicode(pt, &ch);
-
-        pt += n;
-        len -= n;
-
-        add_ubuf(current, ch);
-    }
-    flush_ubuf(current);
-#else
-    DWORD nwritten;
-    COORD pos;
-
-    pos.Y = (SHORT)current->y;
-    pos.X = (SHORT)current->x;
-
-    SetConsoleCursorPosition(current->outh, pos);
-    WriteConsoleA(current->outh, pt, len, &nwritten, 0);
-
-    current->x += len;
-#endif
-
-    sb_clear(current->output);
-
-    return 0;
-}
-
-static int outputChars(struct current *current, const char *buf, int len)
-{
-    if (len < 0) {
-        len = strlen(buf);
-    }
-    assert(current->output);
-
-    sb_append_len(current->output, buf, len);
-
-    return 0;
-}
-
-static void outputNewline(struct current *current)
-{
-    /* On the last row output a newline to force a scroll */
-    if (current->y + 1 == current->rows) {
-        outputChars(current, "\n", 1);
-    }
-    flushOutput(current);
-    current->x = 0;
-    current->y++;
-}
-
-static void setOutputHighlight(struct current *current, const int *props, int nprops)
-{
-    int colour = FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN;
-    int bold = 0;
-    int reverse = 0;
-    int i;
-
-    for (i = 0; i < nprops; i++) {
-        switch (props[i]) {
-            case 0:
-               colour = FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN;
-               bold = 0;
-               reverse = 0;
-               break;
-            case 1:
-               bold = FOREGROUND_INTENSITY;
-               break;
-            case 7:
-               reverse = 1;
-               break;
-            case 30:
-               colour = 0;
-               break;
-            case 31:
-               colour = FOREGROUND_RED;
-               break;
-            case 32:
-               colour = FOREGROUND_GREEN;
-               break;
-            case 33:
-               colour = FOREGROUND_RED | FOREGROUND_GREEN;
-               break;
-            case 34:
-               colour = FOREGROUND_BLUE;
-               break;
-            case 35:
-               colour = FOREGROUND_RED | FOREGROUND_BLUE;
-               break;
-            case 36:
-               colour = FOREGROUND_BLUE | FOREGROUND_GREEN;
-               break;
-            case 37:
-               colour = FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN;
-               break;
-        }
-    }
-
-    flushOutput(current);
-
-    if (reverse) {
-        SetConsoleTextAttribute(current->outh, BACKGROUND_INTENSITY);
-    }
-    else {
-        SetConsoleTextAttribute(current->outh, colour | bold);
-    }
-}
-
-static void eraseEol(struct current *current)
-{
-    COORD pos;
-    DWORD n;
-
-    pos.X = (SHORT) current->x;
-    pos.Y = (SHORT) current->y;
-
-    FillConsoleOutputCharacter(current->outh, ' ', current->cols - current->x, pos, &n);
-}
-
-static void setCursorXY(struct current *current)
-{
-    COORD pos;
-
-    pos.X = (SHORT) current->x;
-    pos.Y = (SHORT) current->y;
-
-    SetConsoleCursorPosition(current->outh, pos);
-}
-
-
-static void setCursorPos(struct current *current, int x)
-{
-    current->x = x;
-    setCursorXY(current);
-}
-
-static void cursorUp(struct current *current, int n)
-{
-    current->y -= n;
-    setCursorXY(current);
-}
-
-static void cursorDown(struct current *current, int n)
-{
-    current->y += n;
-    setCursorXY(current);
-}
-
-static int fd_read(struct current *current)
-{
-    while (1) {
-        INPUT_RECORD irec;
-        DWORD n;
-        if (WaitForSingleObject(current->inh, INFINITE) != WAIT_OBJECT_0) {
-            break;
-        }
-        if (!ReadConsoleInputW(current->inh, &irec, 1, &n)) {
-            break;
-        }
-        if (irec.EventType == KEY_EVENT) {
-            KEY_EVENT_RECORD *k = &irec.Event.KeyEvent;
-            if (k->bKeyDown || k->wVirtualKeyCode == VK_MENU) {
-                if (k->dwControlKeyState & ENHANCED_KEY) {
-                    switch (k->wVirtualKeyCode) {
-                     case VK_LEFT:
-                        return SPECIAL_LEFT;
-                     case VK_RIGHT:
-                        return SPECIAL_RIGHT;
-                     case VK_UP:
-                        return SPECIAL_UP;
-                     case VK_DOWN:
-                        return SPECIAL_DOWN;
-                     case VK_INSERT:
-                        return SPECIAL_INSERT;
-                     case VK_DELETE:
-                        return SPECIAL_DELETE;
-                     case VK_HOME:
-                        return SPECIAL_HOME;
-                     case VK_END:
-                        return SPECIAL_END;
-                     case VK_PRIOR:
-                        return SPECIAL_PAGE_UP;
-                     case VK_NEXT:
-                        return SPECIAL_PAGE_DOWN;
-                    }
-                }
-                /* Note that control characters are already translated in AsciiChar */
-                else if (k->wVirtualKeyCode == VK_CONTROL)
-                    continue;
-                else {
-                    return k->uChar.UnicodeChar;
-                }
-            }
-        }
-    }
-    return -1;
-}
-
-static int getWindowSize(struct current *current)
-{
-    CONSOLE_SCREEN_BUFFER_INFO info;
-    if (!GetConsoleScreenBufferInfo(current->outh, &info)) {
-        return -1;
-    }
-    current->cols = info.dwSize.X;
-    current->rows = info.dwSize.Y;
-    if (current->cols <= 0 || current->rows <= 0) {
-        current->cols = 80;
-        return -1;
-    }
-    current->y = info.dwCursorPosition.Y;
-    current->x = info.dwCursorPosition.X;
-    return 0;
-}
-
-#endif
-
-#if defined(USE_TERMIOS)
 static void linenoiseAtExit(void);
 static struct termios orig_termios; /* in order to restore at exit */
 static int rawmode = 0; /* for atexit() function to check if restore is needed*/
@@ -1783,7 +1387,6 @@ static int check_special(int fd)
 
     return SPECIAL_NONE;
 }
-#endif
 
 static void clearOutputHighlight(struct current *current)
 {
@@ -1847,10 +1450,8 @@ static linenoiseFreeHintsCallback *freeHintsCallback = NULL;
 static void *hintsUserdata = NULL;
 
 static void beep() {
-#ifdef USE_TERMIOS
     fprintf(stderr, "\x7");
     fflush(stderr);
-#endif
 }
 
 static void freeCompletions(linenoiseCompletions *lc) {
@@ -2053,7 +1654,6 @@ static int refreshShowHints(struct current *current, const char *buf, int availc
     return rc;
 }
 
-#ifdef USE_TERMIOS
 static void refreshStart(struct current *current)
 {
     /* We accumulate all output here */
@@ -2082,7 +1682,6 @@ static void refreshNewline(struct current *current)
 static void refreshEndChars(struct current *current)
 {
 }
-#endif
 
 static void refreshLineAlt(struct current *current, const char *prompt, const char *buf, int cursor_pos)
 {
@@ -2176,13 +1775,7 @@ static void refreshLineAlt(struct current *current, const char *prompt, const ch
             }
 
             DRL_CHAR(ch);
-#ifdef USE_WINCONSOLE
-            if (visible) {
-                outputChars(current, pt, n);
-            }
-#else
             outputChars(current, pt, n);
-#endif
         }
         pt += n;
 
@@ -2524,11 +2117,9 @@ static int reverseIncrementalSearch(struct current *current)
             }
             continue;
         }
-#ifdef USE_TERMIOS
         if (c == CHAR_ESCAPE) {
             c = check_special(current->fd);
         }
-#endif
         if (c == ctrl('P') || c == SPECIAL_UP) {
             /* Search for the previous (earlier) match */
             if (searchpos > 0) {
@@ -2628,11 +2219,9 @@ static int linenoiseEdit(struct current *current) {
             /* go on to process the returned char normally */
         }
 
-#ifdef USE_TERMIOS
         if (c == CHAR_ESCAPE) {   /* escape sequence */
             c = check_special(current->fd);
         }
-#endif
         if (c == -1) {
             /* Return on errors */
             return sb_len(current->buf);
@@ -3047,4 +2636,3 @@ char **linenoiseHistory(int *len) {
     }
     return history;
 }
-
