858450b26f6835acd4bd3f51d6d206ad01b885d0
Sebanisu <peeceseven@gmail.com>
2021-02-09 14:55:25 -0500

    re add #define xplus back to ja.h

--- jsrc/ja.h
+++ jsrc/ja.h
@@ -826,7 +826,7 @@
 #define xoinit()                    jtxoinit(jt)
 #define xop(x)                      jtxop(jt,(x))
 #define xop2(x,y,z)                 jtxop2(jt,(x),(y),(z))
-
+#define xplus(x,y)                  jtxplus(jt,(x),(y)) // 5 files use this in macros.
 #define xpow(x,y)                   jtxpow(jt,(x),(y))
 #define xprimetest(x)               jtxprimetest(jt,(x))
 #define xrand(x)                    jtxrand(jt,(x))
