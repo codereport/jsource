9e1cc02dda4b834ddf847be5b2160e24e37589c6
Sebanisu <peeceseven@gmail.com>
2021-02-09 09:52:17 -0500

    remove #define remid

--- jsrc/ja.h
+++ jsrc/ja.h
@@ -573,7 +573,7 @@
 #define redspse(x0,x1,x2,x3,x4,x5,x6,x7)            jtredspse(jt,(x0),(x1),(x2),(x3),(x4),(x5),(x6),(x7))
 #define redspsprep(x0,x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12)  jtredspsprep(jt,(x0),(x1),(x2),(x3),(x4),(x5),(x6),(x7),(x8),(x9),(x10),(x11),(x12))
 
-#define remid(x,y)                  jtremid(jt,(x),(y))
+
 #define rep1d(x0,x1,x2,x3)          jtrep1d(jt,(x0),(x1),(x2),(x3))
 #define rep1sa(x,y,z)               jtrep1sa(jt,(x),(y),(z))
 #define repidx(x0,x1,x2,x3)         jtrepidx(jt,(x0),(x1),(x2),(x3))
