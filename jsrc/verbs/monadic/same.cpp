439e46f19a034aee44762d9b213c94f0a7ff059f
Conor Hoekstra <codereport@outlook.com>
2021-02-10 18:56:46 -0500

    Porting right to same

---
+++ jsrc/verbs/monadic/same.cpp
@@ -0,0 +1,15 @@
+
+#include "array.hpp"
+
+/** @file */
+
+/**
+ * @brief '[' Same
+ * @param jt J Syntax Tree
+ * @param w  Input array
+ * @return   Output array 
+ */
+[[nodiscard]] auto
+same(J jt, array w) -> array {
+    return w;
+}
