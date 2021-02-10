9b5356b596555b8730407fee8c94f5af29c8a27c
Nathan Esau <nathanesau1@gmail.com>
2021-02-09 22:38:25 -0500

    refactor to avoid ITM

--- docs/scripts/preprocessor.py
+++ docs/scripts/preprocessor.py
@@ -15,25 +15,20 @@ preprocessor steps:
 import argparse
 import shutil
 import os
+import itertools
 
 
 def find_markdown_tables(lines):
     """
     return (row_start, row_end) for all markdown tables.
     """
-    tpos = []
-    md_table_idx = None
-    for idx, line in enumerate(lines):
-        if line.startswith("|"):
-            if not md_table_idx:
-                md_table_idx = idx
-        else: # end of md table
-            if md_table_idx is not None:
-                tpos.append((md_table_idx, idx - 1))
-                md_table_idx = None
-    if md_table_idx is not None:
-        tpos.append((md_table_idx, idx))
-    return tpos
+    indices = [idx for idx, line in enumerate(lines) if line.startswith("|")]
+    nindices = len(indices)
+    indices_type = ["end" if i + 1 == nindices or indices[i+1] - indices[i] > 1
+                    else "start" if i == 0 or indices[i] - indices[i-1] > 1
+                    else "mid" for i in range(nindices)]
+    return list(zip([indices[i] for i in range(nindices) if indices_type[i] == "start"],
+                    [indices[i] for i in range(nindices) if indices_type[i] == "end"]))
 
 
 def get_markdown_row_data(row):
@@ -46,10 +41,7 @@ def get_markdown_row_data(row):
     data
         ['apples', 'red', 'a']
     """
-    data = row.split("|")
-    data = data[0:len(data)-1]
-    data = [e.strip() for e in data if e.strip()]
-    return data
+    return [e.strip() for e in row.split("|")[1:-1] if e.strip()]
 
 
 def convert_table(table, in_format="md", out_format="rst-2"):
@@ -59,7 +51,7 @@ def convert_table(table, in_format="md", out_format="rst-2"):
         | ----- | ----- | ---------- |
         | apples | red | a |
         | oranges | orange | o |
-    
+
     rst-1 format
         +---------+--------+------------+
         | fruit   | color  | startswith |
@@ -77,30 +69,26 @@ def convert_table(table, in_format="md", out_format="rst-2"):
         oranges  orange o
         =======  =====  ==========
     """
-    if in_format == "md" and out_format=="rst-2":
+    if in_format == "md" and out_format == "rst-2":
         header = get_markdown_row_data(table[0])
-        # NOTE: skip table[1]
         rows = [get_markdown_row_data(row) for row in table[2:]]
-        cols = [[row[col_idx] for row in rows] for col_idx in range(len(header))]
-        rst_table = []
-        separator = ' '.join(
-            ['=' * max(len(header[col_idx]), len(max(col, key=len))) for col_idx, col in enumerate(cols)],
+        cols = [[row[col_idx] for row in rows]
+                for col_idx in range(len(header))]
+        tbl_separator = ' '.join(
+            ['=' * max(len(header[col_idx]), len(max(col, key=len)))
+             for col_idx, col in enumerate(cols)],
         )
-        rst_table.append(separator)
-        data = []
-        for col_idx, col in enumerate(cols):
-            length = max(len(header[col_idx]), len(max(col, key=len)))
-            data.append(header[col_idx] + ' ' * (length - len(header[col_idx])))
-        rst_table.append(' '.join(data))
-        rst_table.append(separator)
-        for row in rows:
-            data = []
-            for col_idx, col in enumerate(cols):
-                length = max(len(header[col_idx]), len(max(col, key=len)))
-                data.append(row[col_idx] + ' ' * (length - len(row[col_idx])))
-            rst_table.append(' '.join(data))
-        rst_table.append(separator)
-        return rst_table
+        tbl_header = ' '.join([
+            header[idx] + ' ' * (max(len(header[idx]), len(max(col, key=len))) - len(header[idx]))
+            for idx, col in enumerate(cols)
+        ])
+        tbl_rows = [
+            ' '.join([
+                row[idx] + ' ' * (max(len(header[idx]), len(max(col, key=len))) - len(row[idx]))
+                for idx, col in enumerate(cols)
+            ]) for row in rows
+        ]
+        return [tbl_separator] + [tbl_header] + [tbl_separator] + tbl_rows + [tbl_separator]
 
     raise Exception("Conversion from {} to {} not implemented".format(
         in_format, out_format
@@ -110,18 +98,24 @@ def convert_table(table, in_format="md", out_format="rst-2"):
 def get_processed_lines(lines):
     """
     replace all md tables in lines with rst-2 tables
-    
+
     - additional processing can be added later if needed
     """
     tpos = find_markdown_tables(lines)
-    for pos in tpos:
-        md_table = lines[pos[0]:pos[1]+1]
-        rst_table = convert_table(md_table, in_format="md", out_format="rst-2")
-        # replace md_table with rst_table
-        lines = lines[0:pos[0]] + \
-            rst_table + \
-            lines[pos[1]+1:]
-    return lines
+    
+    if not tpos:
+        return lines
+    
+    chunks = [[(0, tpos[0][0] - 1)]] + \
+        [[pos] if idx == 0
+         else [(tpos[idx-1][1] + 1, pos[0] - 1), pos] for idx, pos in enumerate(tpos)] + \
+        [[(tpos[-1][1] + 1, len(lines) - 1)]]
+    
+    return list(itertools.chain.from_iterable([
+        convert_table(lines[chunk[0]:chunk[1]+1], "md", "rst-2") if chunk in tpos \
+        else lines[chunk[0]:chunk[1]+1] \
+        for chunk in list(itertools.chain(*chunks))
+    ]))
 
 
 if __name__ == "__main__":
@@ -143,9 +137,8 @@ if __name__ == "__main__":
             if file.endswith('.rst'):
                 with open('{}/{}'.format(root, file)) as f:
                     data = f.read()
-                lines = data.splitlines()
-                lines = get_processed_lines(lines)
+                lines = get_processed_lines(data.splitlines())
                 with open('{}/{}'.format(root, file), 'w') as f:
                     f.write('\n'.join(lines))
-    
+
     print("preprocessor step finished")
