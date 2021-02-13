c68ceca33601dd4e98e222526525acd01d6cff13
Juho Eerola <juho.eerola@tuni.fi>
2021-02-13 10:06:46 +0200

    Clang-format c++ files and headers

--- jsrc/util/string.cpp
+++ jsrc/util/string.cpp
@@ -1,11 +1,10 @@
 
 #include "string.hpp"
 
-
-namespace util{
-    auto remove_all_occurrences(std::string &str, std::string_view sub_str) -> void {
-        size_t n = 0;
-        while((n = str.find(sub_str, n)) != std::string::npos)
-            str.erase(n,sub_str.size());
-    }
+namespace util {
+auto
+remove_all_occurrences(std::string &str, std::string_view sub_str) -> void {
+    size_t n = 0;
+    while ((n = str.find(sub_str, n)) != std::string::npos) str.erase(n, sub_str.size());
 }
+}  // namespace util
