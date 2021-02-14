6cdac6831995028fa806c1aa963f7c8ffaa640af
Herwin Weststrate <herwin@snt.utwente.nl>
2021-02-14 13:40:58 +0100

    Expand defined in base64 codecs

--- base64/lib/arch/generic/codec-generic.c
+++ base64/lib/arch/generic/codec-generic.c
@@ -9,14 +9,28 @@
 
 #include "64/enc_loop.c"
 
-BASE64_ENC_FUNCTION(plain)
+void
+base64_stream_encode_plain
+	( struct base64_state	*state
+	, const char		*src
+	, size_t		 srclen
+	, char			*out
+	, size_t		*outlen
+	)
 {
 	#include "enc_head.c"
 	enc_loop_generic_64(&s, &slen, &o, &olen);
 	#include "enc_tail.c"
 }
 
-BASE64_DEC_FUNCTION(plain)
+int
+base64_stream_decode_plain
+	( struct base64_state	*state
+	, const char		*src
+	, size_t		 srclen
+	, char			*out
+	, size_t		*outlen
+	)
 {
 	#include "dec_head.c"
 	#include "dec_tail.c"
--- base64/lib/codecs.h
+++ base64/lib/codecs.h
@@ -3,36 +3,20 @@
 
 #include "../include/libbase64.h"
 
-// Function parameters for encoding functions:
-#define BASE64_ENC_PARAMS			\
-	( struct base64_state	*state		\
-	, const char		*src		\
-	, size_t		 srclen		\
-	, char			*out		\
-	, size_t		*outlen		\
-	)
+void
+base64_stream_encode_plain
+	( struct base64_state	*state
+	, const char		*src
+	, size_t		 srclen
+	, char			*out
+	, size_t		*outlen
+	);
 
-// Function parameters for decoding functions:
-#define BASE64_DEC_PARAMS			\
-	( struct base64_state	*state		\
-	, const char		*src		\
-	, size_t		 srclen		\
-	, char			*out		\
-	, size_t		*outlen		\
-	)
-
-// Function signature for encoding functions:
-#define BASE64_ENC_FUNCTION(arch)		\
-	void					\
-	base64_stream_encode_ ## arch		\
-	BASE64_ENC_PARAMS
-
-// Function signature for decoding functions:
-#define BASE64_DEC_FUNCTION(arch)		\
-	int					\
-	base64_stream_decode_ ## arch		\
-	BASE64_DEC_PARAMS
-
-// Function declarations:
-BASE64_ENC_FUNCTION(plain);
-BASE64_DEC_FUNCTION(plain);
+int
+base64_stream_decode_plain
+	( struct base64_state	*state
+	, const char		*src
+	, size_t		 srclen
+	, char			*out
+	, size_t		*outlen
+	);
