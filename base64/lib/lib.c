3e14ce5d9d557226b2a8fe9c6f697c5f04e78cf7
Herwin Weststrate <herwin@snt.utwente.nl>
2021-02-17 15:07:08 +0100

    Clang-format the base64 library
    This has been omitted from #148, most likely because there were open
    merge requests.

--- base64/lib/lib.c
+++ base64/lib/lib.c
@@ -8,10 +8,9 @@
 /* Call this before calling base64_stream_encode() to init the state. See above
  */
 static void
-base64_stream_encode_init (struct base64_state *state)
-{
-	state->bytes = 0;
-	state->carry = 0;
+base64_stream_encode_init(struct base64_state *state) {
+    state->bytes = 0;
+    state->carry = 0;
 }
 
 /* Finalizes the output begun by previous calls to `base64_stream_encode()`.
@@ -19,84 +18,64 @@ base64_stream_encode_init (struct base64_state *state)
  * and will contain the number of new bytes written at `out` (which will quite
  * often be zero). */
 static void
-base64_stream_encode_final
-	( struct base64_state	*state
-	, char			*out
-	, size_t		*outlen
-	)
-{
-	uint8_t *o = (uint8_t *)out;
+base64_stream_encode_final(struct base64_state *state, char *out, size_t *outlen) {
+    uint8_t *o = (uint8_t *)out;
 
-	if (state->bytes == 1) {
-		*o++ = base64_table_enc_6bit[state->carry];
-		*o++ = '=';
-		*o++ = '=';
-		*outlen = 3;
-		return;
-	}
-	if (state->bytes == 2) {
-		*o++ = base64_table_enc_6bit[state->carry];
-		*o++ = '=';
-		*outlen = 2;
-		return;
-	}
-	*outlen = 0;
+    if (state->bytes == 1) {
+        *o++    = base64_table_enc_6bit[state->carry];
+        *o++    = '=';
+        *o++    = '=';
+        *outlen = 3;
+        return;
+    }
+    if (state->bytes == 2) {
+        *o++    = base64_table_enc_6bit[state->carry];
+        *o++    = '=';
+        *outlen = 2;
+        return;
+    }
+    *outlen = 0;
 }
 
 /* Call this before calling base64_stream_decode() to init the state. See above
  */
 static void
-base64_stream_decode_init (struct base64_state *state)
-{
-	state->bytes = 0;
-	state->carry = 0;
+base64_stream_decode_init(struct base64_state *state) {
+    state->bytes = 0;
+    state->carry = 0;
 }
 
 void
-base64_encode
-	( const char	*src
-	, size_t	 srclen
-	, char		*out
-	, size_t	*outlen
-	)
-{
-	size_t s;
-	size_t t;
-	struct base64_state state;
+base64_encode(const char *src, size_t srclen, char *out, size_t *outlen) {
+    size_t s;
+    size_t t;
+    struct base64_state state;
 
-	// Init the stream reader:
-	base64_stream_encode_init(&state);
+    // Init the stream reader:
+    base64_stream_encode_init(&state);
 
-	// Feed the whole string to the stream reader:
-	base64_stream_encode(&state, src, srclen, out, &s);
+    // Feed the whole string to the stream reader:
+    base64_stream_encode(&state, src, srclen, out, &s);
 
-	// Finalize the stream by writing trailer if any:
-	base64_stream_encode_final(&state, out + s, &t);
+    // Finalize the stream by writing trailer if any:
+    base64_stream_encode_final(&state, out + s, &t);
 
-	// Final output length is stream length plus tail:
-	*outlen = s + t;
+    // Final output length is stream length plus tail:
+    *outlen = s + t;
 }
 
 int
-base64_decode
-	( const char	*src
-	, size_t	 srclen
-	, char		*out
-	, size_t	*outlen
-	)
-{
-	int ret;
-	struct base64_state state;
+base64_decode(const char *src, size_t srclen, char *out, size_t *outlen) {
+    int ret;
+    struct base64_state state;
 
-	// Init the stream reader:
-	base64_stream_decode_init(&state);
+    // Init the stream reader:
+    base64_stream_decode_init(&state);
 
-	// Feed the whole string to the stream reader:
-	ret = base64_stream_decode(&state, src, srclen, out, outlen);
+    // Feed the whole string to the stream reader:
+    ret = base64_stream_decode(&state, src, srclen, out, outlen);
 
-	// If when decoding a whole block, we're still waiting for input then fail:
-	if (ret && (state.bytes == 0)) {
-		return ret;
-	}
-	return 0;
+    // If when decoding a whole block, we're still waiting for input then fail:
+    if (ret && (state.bytes == 0)) { return ret; }
+    return 0;
 }
