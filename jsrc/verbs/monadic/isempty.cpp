6392a5afb2a002b234de3660f8fb0e3f57587855
Herwin Weststrate <herwin@snt.utwente.nl>
2021-02-21 11:30:45 +0100

    Improve documentation for jtisempty

--- jsrc/verbs/monadic/isempty.cpp
+++ jsrc/verbs/monadic/isempty.cpp
@@ -1,7 +1,15 @@
 #include "array.hpp"
 
+/** @file */
+
+/**
+ * @brief `0 e. $` IsEmpty
+ * @param jt       JST (J Syntax Tree)
+ * @param w        Input aray
+ * @return         boolean jtrue or jfalse
+ */
 [[nodiscard]] auto
 jtisempty(J jt, array w) -> array {
     if ((AT(w) & SPARSE) != 0) return jteps(jt, num(0), shape(jt, w));
     return AN(w) == 0 ? jtrue : jfalse;
-}  // 0 e. $
+}
