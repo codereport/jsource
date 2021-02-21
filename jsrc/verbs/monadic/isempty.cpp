8ba4852388310c325cfa26229b4c0ea377c9d80a
Herwin Weststrate <herwin@snt.utwente.nl>
2021-02-21 11:28:22 +0100

    Restructure jtisempty to use preferred layout
    * Replace `A` with `array`
    * Use trailing return types

--- jsrc/verbs/monadic/isempty.cpp
+++ jsrc/verbs/monadic/isempty.cpp
@@ -1,7 +1,7 @@
 #include "array.hpp"
 
-[[nodiscard]] A
-jtisempty(J jt, A w) {
+[[nodiscard]] auto
+jtisempty(J jt, array w) -> array {
     if ((AT(w) & SPARSE) != 0) return jteps(jt, num(0), shape(jt, w));
     return AN(w) == 0 ? jtrue : jfalse;
 }  // 0 e. $
