9bec2a82f59e0518c0994121fc7c9dd3c0883351
Herwin Weststrate <herwin@snt.utwente.nl>
2021-02-21 11:57:30 +0100

    Use is_sparse method

--- jsrc/verbs/monadic/isempty.cpp
+++ jsrc/verbs/monadic/isempty.cpp
@@ -10,6 +10,6 @@
  */
 [[nodiscard]] auto
 jtisempty(J jt, array w) -> array {
-    if ((AT(w) & SPARSE) != 0) return jteps(jt, num(0), shape(jt, w));
+    if (is_sparse(w)) return jteps(jt, num(0), shape(jt, w));
     return AN(w) == 0 ? jtrue : jfalse;
 }
