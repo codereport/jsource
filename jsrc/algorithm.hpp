33650854d501ad74fd8a9a9aaa347d617b2bb79d
Sebanisu <peeceseven@gmail.com>
2021-02-25 20:38:12 -0500

    so clangformat won't put a new line after east constexpr.
    So I put auto at the far right so it's doing the newline.

--- jsrc/algorithm.hpp
+++ jsrc/algorithm.hpp
@@ -23,7 +23,8 @@ static auto constexpr not_equal_to = [](auto const &first, auto const &...values
  * @brief Variadic version of `std::mismatch`
  */
 template <typename P, typename I, typename... Is>
-[[nodiscard]] static auto constexpr zip_find(P const &pred, I f, I const l, Is... fs) {
+[[nodiscard]] static constexpr auto
+zip_find(P const &pred, I f, I const l, Is... fs) {
     static_assert(sizeof...(Is) != 0);
     while (f != l) {
         if (pred(*f, *fs...)) break;
@@ -36,7 +37,8 @@ template <typename P, typename I, typename... Is>
  * @brief Predicate version of `algo::zip_found`
  */
 template <typename P, typename I, typename... Is>
-[[nodiscard]] static auto constexpr zip_found(P const &pred, I f, I const l, Is... fs) -> bool {
+[[nodiscard]] static constexpr auto
+zip_found(P const &pred, I f, I const l, Is... fs) -> bool {
     auto const t = zip_find(pred, f, l, fs...);
     return std::get<0>(t) != l;
 }
@@ -57,7 +59,8 @@ template <typename P, typename I, typename... Is>
  * @brief Variadic, Predicate version of `std::mismatch`
  */
 template <typename I, typename... Is>
-[[nodiscard]] static auto constexpr is_mismatched(I f, I const l, Is... fs) -> bool {
+[[nodiscard]] static constexpr auto
+is_mismatched(I f, I const l, Is... fs) -> bool {
     return zip_found(not_equal_to, f, l, fs...);
 }
 
@@ -77,7 +80,8 @@ template <typename I, typename... Is>
  * @brief Variadic, Predicate version of `std::equal`
  */
 template <typename I, typename... Is>
-[[nodiscard]] static auto constexpr is_equal(I f, I const l, Is... fs) -> bool {
+[[nodiscard]] static constexpr auto
+is_equal(I f, I const l, Is... fs) -> bool {
     return !is_mismatched(f, l, fs...);
 }
 
@@ -87,7 +91,8 @@ namespace ranges {
  * @note This can be the same name as zip_find if we use concepts.
  */
 template <typename P, typename R, typename... Rs>
-[[nodiscard]] static auto constexpr zip_find(P const &pred, R const &r, Rs const &...rs) {
+[[nodiscard]] static constexpr auto
+zip_find(P const &pred, R const &r, Rs const &...rs) {
     return algo::zip_find(pred, std::cbegin(r), std::cend(r), std::cbegin(rs)...);
 }
 /**
@@ -95,14 +100,16 @@ template <typename P, typename R, typename... Rs>
  * @note This can be the same name as zip_found if we use concepts.
  */
 template <typename P, typename R, typename... Rs>
-[[nodiscard]] static auto constexpr zip_found(P const &pred, R const &r, Rs const &...rs) -> bool {
+[[nodiscard]] static constexpr auto
+zip_found(P const &pred, R const &r, Rs const &...rs) -> bool {
     return algo::zip_found(pred, std::cbegin(r), std::cend(r), std::cbegin(rs)...);
 }
 /**
  * @brief Variadic, Predicate version of `std::mismatch`, for ranges;
  */
 template <typename R, typename... Rs>
-[[nodiscard]] static auto constexpr is_mismatched(R const &r, Rs const &...rs) -> bool {
+[[nodiscard]] static constexpr auto
+is_mismatched(R const &r, Rs const &...rs) -> bool {
     return not_equal_to(std::size(r), std::size(rs)...) ||
            algo::is_mismatched(std::cbegin(r), std::cend(r), std::cbegin(rs)...);
 }
@@ -110,7 +117,8 @@ template <typename R, typename... Rs>
  * @brief Variadic, Predicate version of `std::equal`, for ranges;
  */
 template <typename... Rs>
-[[nodiscard]] static auto constexpr is_equal(Rs const &...rs) -> bool {
+[[nodiscard]] static constexpr auto
+is_equal(Rs const &...rs) -> bool {
     return !is_mismatched(rs...);
 }
 // static_assert tests. With constexpr we can start checking to make sure our functions work at compile time.
