c00675b3215a65ed87db30f1b558060ac60f3057
Sebanisu <peeceseven@gmail.com>
2021-02-25 20:24:03 -0500

    Update algorithm.hpp
    I thought the variadic and the 2 argument version would be ambiguous.
    
    added a 2 arg version of is_equal
    
    I commented out the 2 arg versions though because they take priority over the variadic versions. And if they are called in a constexpr  context pre cpp20 they fail to compile.
    
    ran clangformat via clion. it seems to work.
    
    added a 2 arg static_assert test.
    
    const all the end iterators. since they won't change.

--- jsrc/algorithm.hpp
+++ jsrc/algorithm.hpp
@@ -23,7 +23,7 @@ static auto constexpr not_equal_to = [](auto const &first, auto const &...values
  * @brief Variadic version of `std::mismatch`
  */
 template <typename P, typename I, typename... Is>
-[[nodiscard]] static auto constexpr zip_find(P const &pred, I f, I l, Is... fs) {
+[[nodiscard]] static auto constexpr zip_find(P const &pred, I f, I const l, Is... fs) {
     static_assert(sizeof...(Is) != 0);
     while (f != l) {
         if (pred(*f, *fs...)) break;
@@ -36,34 +36,48 @@ template <typename P, typename I, typename... Is>
  * @brief Predicate version of `algo::zip_found`
  */
 template <typename P, typename I, typename... Is>
-[[nodiscard]] static auto constexpr zip_found(P const &pred, I f, I l, Is... fs) -> bool {
+[[nodiscard]] static auto constexpr zip_found(P const &pred, I f, I const l, Is... fs) -> bool {
     auto const t = zip_find(pred, f, l, fs...);
     return std::get<0>(t) != l;
 }
 
-/**
- * @brief Predicate version of `std::mismatch`
- * @note std::mismatch isn't constexpr till cpp20
- */
-template <typename I, typename I2>
-[[nodiscard]] static auto
-is_mismatched(I f, I l, I2 f2) -> bool {
-    return std::mismatch(f, l, f2).first != l;
-}
+// This 2 arg function takes priority so will not work when called from constexpr functions. This isn't an issue in
+// cpp20.
+// /**
+//  * @brief Predicate version of `std::mismatch`
+//  * @note std::mismatch isn't constexpr till cpp20
+//  */
+// template <typename I, typename I2>
+// [[nodiscard]] static auto
+// is_mismatched(I f, I const l, I2 f2) -> bool {
+//     return std::mismatch(f, l, f2).first != l;
+// }
 
 /**
  * @brief Variadic, Predicate version of `std::mismatch`
  */
 template <typename I, typename... Is>
-[[nodiscard]] static auto constexpr is_mismatched(I f, I l, Is... fs) -> bool {
+[[nodiscard]] static auto constexpr is_mismatched(I f, I const l, Is... fs) -> bool {
     return zip_found(not_equal_to, f, l, fs...);
 }
+
+// // This 2 arg function takes priority so will not work when called from constexpr functions. This isn't an issue in
+// cpp20.
+// /**
+//  * @brief Predicate version of `std::equal`
+//  * @note std::equal isn't constexpr till cpp20
+//  */
+// template <typename I, typename I2>
+// [[nodiscard]] static auto
+// is_equal(I f, I const l, I2 f2) -> bool {
+//     return std::equal(f, l, f2);
+// }
 
 /**
  * @brief Variadic, Predicate version of `std::equal`
  */
 template <typename I, typename... Is>
-[[nodiscard]] static auto constexpr is_equal(I f, I l, Is... fs) -> bool {
+[[nodiscard]] static auto constexpr is_equal(I f, I const l, Is... fs) -> bool {
     return !is_mismatched(f, l, fs...);
 }
 
@@ -112,5 +126,6 @@ static_assert(is_mismatched(std::array{0, 1, 2, 5, 4}, std::array{0, 1, 2, 3, 4}
 static_assert(is_mismatched(std::array{0, 1, 2, 5, 4}, std::array{0, 1, 2, 3}, std::array{0, 1, 2, 3, 6}));
 static_assert(!is_mismatched(std::array{0, 1, 2, 3}, std::array{0, 1, 2, 3}, std::array{0, 1, 2, 3}));
 static_assert(is_equal(std::array{0, 1, 2, 3}, std::array{0, 1, 2, 3}, std::array{0, 1, 2, 3}));
+static_assert(ranges::is_equal(std::array{0, 1, 2, 3}, std::array{0, 1, 2, 3}));
 }  // namespace ranges
 }  // namespace algo
