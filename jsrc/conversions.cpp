fe73293a4436b30975a841c4e6aae84506e51139
Juho Eerola <juho.eerola@tuni.fi>
2021-03-07 02:53:02 +0200

    Replace `*AV()` macros with `pointer_to_values()`

--- jsrc/conversions.cpp
+++ jsrc/conversions.cpp
@@ -35,7 +35,7 @@ in_range() -> bool {
 template <typename From, typename To>
 [[nodiscard]] auto
 convert(J jt, array w, void *yv) -> bool {
-    From *v = reinterpret_cast<From *>(UAV(w));
+    auto *v = pointer_to_values<From>(w);
     if constexpr (!in_range<To, From>()) {
         // TODO: replace with short circuiting solution
         auto out = static_cast<To *>(yv);
@@ -48,7 +48,7 @@ convert(J jt, array w, void *yv) -> bool {
 template <typename From, typename To, typename Transform>
 [[nodiscard]] auto
 convert(J jt, array w, void *yv, Transform t) -> bool {
-    From *v = reinterpret_cast<From*>(UAV(w));
+    auto *v = pointer_to_values<From>(w);
     std::transform(v, v + AN(w), static_cast<To*>(yv), t);
     return 1;
 }
@@ -57,7 +57,7 @@ template <>
 [[nodiscard]] auto
 convert<D, bool>(J jt, A w, void *yv, D fuzz) -> bool {
     auto n = AN(w);
-    auto v = DAV(w);
+    auto v = pointer_to_values<double>(w);
     auto x = (B *)yv;
     DQ(n, auto p = *v++; if (p < -2 || 2 < p) return 0;  // handle infinities
        I val = 2;
@@ -72,7 +72,7 @@ template <>
 [[nodiscard]] auto
 convert<D, I>(J jt, A w, void *yv, D fuzz) -> bool {
     auto n = AN(w);
-    auto v = DAV(w);
+    auto v = pointer_to_values<double>(w);
     auto x = (I *)yv;
     for (int64_t i = 0; i < n; ++i) {
         auto const p = v[i];
@@ -97,7 +97,7 @@ template <>
 [[nodiscard]] auto
 convert<Z, D>(J jt, A w, void *yv, D fuzz) -> bool {
     auto const n  = AN(w);
-    auto const *v = ZAV(w);
+    auto const *v = pointer_to_values<Z>(w);
     auto x        = (D *)yv;
     if (fuzz)
         DQ(
@@ -172,7 +172,7 @@ jtxd1(J jt, D p, I mode) {
     if (p == -inf) return jtvci(jt, XNINF);
     A t;
     GAT0(t, INT, 30, 1);
-    auto u = AV(t);
+    auto u = pointer_to_values(t);
     int64_t m = 0;
     auto d = std::abs(p);
     while (0 < d) {
@@ -182,7 +182,7 @@ jtxd1(J jt, D p, I mode) {
         d            = q;
         if (m == AN(t)) {
             RZ(t = jtext(jt, 0, t));
-            u = AV(t);
+            u = pointer_to_values(t);
         }
     }
     if (!m) {
@@ -204,9 +204,9 @@ convert<D, X>(J jt, A w, void *yv, I mode) -> bool {
 template <>
 [[nodiscard]] auto
 convert<X, bool>(J jt, A w, void *yv) -> bool {
-    auto v = XAV(w);
+    auto v = pointer_to_values<X>(w);
     auto x = (B *)yv;
-    DO(AN(w), A q = v[i]; I e = AV(q)[0]; if ((AN(q) ^ 1) | (e & -2)) return 0; x[i] = (B)e;);
+    DO(AN(w), A q = v[i]; I e = pointer_to_values(q)[0]; if ((AN(q) ^ 1) | (e & -2)) return 0; x[i] = (B)e;);
     return 1;
 }
 
@@ -214,14 +214,14 @@ template <typename T>
 [[nodiscard]] static auto
 value_from_X(X p) -> T {
         auto const n = AN(p);
-        auto const v = std::reverse_iterator(AV(p) + n);
+        auto const v = std::reverse_iterator(pointer_to_values(p) + n);
         return std::accumulate(v, v + n, T{}, [](auto d, auto v) { return v + d * XBASE; });
 }
 
 template <>
 [[nodiscard]] auto
 convert<X, I>(J jt, A w, void *yv) -> bool {
-    auto v = XAV(w);
+    auto v = pointer_to_values<X>(w);
     auto x = (I *)yv;
     auto n = AN(w);
     X p, q;
@@ -239,7 +239,7 @@ template <>
 [[nodiscard]] auto
 convert<X, D>(J jt, A w, void *yv) -> bool {
     return convert<X, D>(jt, w, yv, [](auto p) {
-        auto const c = AV(p)[AN(p) - 1];
+        auto const c = pointer_to_values(p)[AN(p) - 1];
         if (c == XPINF) return inf;
         if (c == XNINF) return infm;
         return value_from_X<double>(p);
@@ -257,7 +257,7 @@ template <>
 convert<D, Q>(J jt, A w, void *yv, I mode) -> bool {
     if (!(w)) return 0;
     auto const n  = AN(w);
-    auto const wv = DAV(w);
+    auto const wv = pointer_to_values<double>(w);
     auto x        = (Q *)yv;
     D t;
     auto tv = 3 + (S *)&t;
@@ -294,7 +294,7 @@ convert<D, Q>(J jt, A w, void *yv, I mode) -> bool {
                 q.d = jtca(jt, iv1);
             }
         }
-        if (neg) inplace_negate(AV(q.n), AN(q.n));
+        if (neg) inplace_negate(pointer_to_values(q.n), AN(q.n));
         *x++ = q;
     }
     return !jt->jerr;
@@ -305,7 +305,7 @@ template <>
 convert<Q, D>(J jt, A w, void *yv) -> bool {
     auto const xb = (D)XBASE;
     auto const wn = AN(w);
-    auto const wv = QAV(w);
+    auto const wv = pointer_to_values<Q>(w);
     auto const x  = (D *)yv;
     auto const nn = 308 / XBASEN;
 
@@ -321,7 +321,7 @@ convert<Q, D>(J jt, A w, void *yv) -> bool {
     for (int64_t i = 0; i < wn; ++i) {
         auto const p  = wv[i].n;
         auto const pn = AN(p);
-        auto const k  = 1 == pn ? AV(p)[0] : 0;
+        auto const k  = 1 == pn ? pointer_to_values(p)[0] : 0;
         auto const q  = wv[i].d;
         auto const qn = AN(q);
         if (k == XPINF)
@@ -329,8 +329,8 @@ convert<Q, D>(J jt, A w, void *yv) -> bool {
         else if (k == XNINF)
             x[i] = infm;
         else if (pn <= nn && qn <= nn) {
-            auto const n = add_digits(pn, AV(p));
-            auto const d = add_digits(qn, AV(q));
+            auto const n = add_digits(pn, pointer_to_values(p));
+            auto const d = add_digits(qn, pointer_to_values(q));
             x[i]         = n / d;
         } else {
             if (!x2)
@@ -341,7 +341,7 @@ convert<Q, D>(J jt, A w, void *yv) -> bool {
             auto const cn = AN(c);
             auto const m  = MIN(cn, 5);
             auto const r  = cn - (m + k);
-            auto const v  = AV(c) + cn - m;
+            auto const v  = pointer_to_values(c) + cn - m;
             auto const n  = add_digits(m, v);
             auto d        = std::pow(xb, std::abs(r));
             x[i]          = 0 > r ? n / d : n * d;
@@ -353,7 +353,7 @@ convert<Q, D>(J jt, A w, void *yv) -> bool {
 template <>
 [[nodiscard]] auto
 convert<Q, X>(J jt, A w, void *yv) -> bool {
-    auto v = QAV(w);
+    auto v = pointer_to_values<Q>(w);
     auto x = (X *)yv;
     DQ(AN(w), if (!(jtequ(jt, iv1, v->d))) return 0; *x++ = v->n; ++v;);
     return !jt->jerr;
@@ -369,7 +369,7 @@ set_real_part(Z *z, int64_t n, T *t) {
 template <>
 [[nodiscard]] auto
 convert<D, Z>(J jt, A w, void *yv) -> bool {
-    set_real_part(static_cast<Z*>(yv), AN(w), DAV(w));
+    set_real_part(static_cast<Z*>(yv), AN(w), pointer_to_values<double>(w));
     return 1;
 }
 
@@ -399,8 +399,8 @@ jtccvt(J jt, I tflagged, A w, A *y) {
             case 3:        // sparse to sparse
                 t1 = DTYPE(t);
                 GASPARSE(*y, t, 1, r, s);
-                yp = PAV(*y);
-                wp = PAV(w);
+                yp = pointer_to_values<P>(*y);
+                wp = pointer_to_values<P>(w);
                 SPB(yp, a, jtca(jt, SPA(wp, a)));
                 SPB(yp, i, jtca(jt, SPA(wp, i)));
                 SPB(yp, e, jtcvt(jt, t1, SPA(wp, e)));
@@ -425,7 +425,7 @@ jtccvt(J jt, I tflagged, A w, A *y) {
     auto n = AN(w);
     A d;
     GA(d, t, n, r, s);
-    auto yv = voidAV(d);  // allocate the same # atoms, even if we will convert fewer
+    auto yv = pointer_to_values<void>(d);  // allocate the same # atoms, even if we will convert fewer
     if (tflagged & NOUNCVTVALIDCT) {
         I inputn = *(I *)y;  // fetch input, in case it is called for
         if (inputn > 0) {    // if converting the leading values, just update the counts
@@ -441,7 +441,7 @@ jtccvt(J jt, I tflagged, A w, A *y) {
     // as n or s[0] since only n atoms will be used.  For higher ranks, we need the shape from s.  So it's just as well
     // that we take the shape from s now
     *y      = d;
-    auto wv = voidAV(w);                       // return the address of the new block
+    auto wv = pointer_to_values<void>(w);                       // return the address of the new block
     if (t & CMPX) jtfillv(jt, t, n, (C *)yv);  // why??  just fill in imaginary parts as we need to
     if (!n) return 1;
     // Perform the conversion based on data types
@@ -467,12 +467,12 @@ jtccvt(J jt, I tflagged, A w, A *y) {
         case CVCASE(XNUMX, B01X): return convert<bool, X>(jt, w, yv);
         case CVCASE(RATX, B01X):
             GATV(d, XNUM, n, r, s);
-            return convert<bool, X>(jt, w, AV(d)) && convert<X, Q>(jt, d, yv);
+            return convert<bool, X>(jt, w, pointer_to_values(d)) && convert<X, Q>(jt, d, yv);
         case CVCASE(FLX, B01X): std::copy_n(static_cast<B *>(wv), n, static_cast<D *>(yv)); return 1;
         case CVCASE(CMPXX, B01X): set_real_part(static_cast<Z*>(yv), n, static_cast<B*>(wv)); return 1;
         case CVCASE(B01X, INTX): return convert<I, bool>(jt, w, yv);
         case CVCASE(XNUMX, INTX): return convert<I, X>(jt, w, yv);
-        case CVCASE(RATX, INTX): GATV(d, XNUM, n, r, s); return convert<I, X>(jt, w, AV(d)) && convert<X, Q>(jt, d, yv);
+        case CVCASE(RATX, INTX): GATV(d, XNUM, n, r, s); return convert<I, X>(jt, w, pointer_to_values(d)) && convert<X, Q>(jt, d, yv);
         case CVCASE(FLX, INTX): std::copy_n(static_cast<I *>(wv), n, static_cast<D *>(yv)); return 1;
         case CVCASE(CMPXX, INTX): set_real_part(static_cast<Z *>(yv), n, static_cast<I *>(wv)); return 1;
         case CVCASE(B01X, FLX): return convert<D, bool>(jt, w, yv, (I)jtinplace & JTNOFUZZ ? 0.0 : FUZZ);
@@ -486,20 +486,20 @@ jtccvt(J jt, I tflagged, A w, A *y) {
         case CVCASE(CMPXX, FLX): return convert<D, Z>(jt, w, yv);
         case CVCASE(B01X, CMPXX):
             GATV(d, FL, n, r, s);
-            if (!(convert<Z, D>(jt, w, AV(d), (I)jtinplace & JTNOFUZZ ? 0.0 : FUZZ))) return 0;
+            if (!(convert<Z, D>(jt, w, pointer_to_values(d), (I)jtinplace & JTNOFUZZ ? 0.0 : FUZZ))) return 0;
             return convert<D, bool>(jt, d, yv, (I)jtinplace & JTNOFUZZ ? 0.0 : FUZZ);
         case CVCASE(INTX, CMPXX):
             GATV(d, FL, n, r, s);
-            if (!(convert<Z, D>(jt, w, AV(d), (I)jtinplace & JTNOFUZZ ? 0.0 : FUZZ))) return 0;
+            if (!(convert<Z, D>(jt, w, pointer_to_values(d), (I)jtinplace & JTNOFUZZ ? 0.0 : FUZZ))) return 0;
             return convert<D, I>(jt, d, yv, (I)jtinplace & JTNOFUZZ ? 0.0 : FUZZ);
         case CVCASE(XNUMX, CMPXX):
             GATV(d, FL, n, r, s);
-            if (!(convert<Z, D>(jt, w, AV(d), (I)jtinplace & JTNOFUZZ ? 0.0 : FUZZ))) return 0;
+            if (!(convert<Z, D>(jt, w, pointer_to_values(d), (I)jtinplace & JTNOFUZZ ? 0.0 : FUZZ))) return 0;
             return convert<D, X>(
               jt, d, yv, (jt->xmode & REPSGN(SGNIFNOT(tflagged, XCVTXNUMORIDEX))) | (tflagged >> XCVTXNUMCVX));
         case CVCASE(RATX, CMPXX):
             GATV(d, FL, n, r, s);
-            if (!(convert<Z, D>(jt, w, AV(d), (I)jtinplace & JTNOFUZZ ? 0.0 : FUZZ))) return 0;
+            if (!(convert<Z, D>(jt, w, pointer_to_values(d), (I)jtinplace & JTNOFUZZ ? 0.0 : FUZZ))) return 0;
             return convert<D, Q>(
               jt, d, yv, (jt->xmode & REPSGN(SGNIFNOT(tflagged, XCVTXNUMORIDEX))) | (tflagged >> XCVTXNUMCVX));
         case CVCASE(FLX, CMPXX): return convert<Z, D>(jt, w, yv, (I)jtinplace & JTNOFUZZ ? 0.0 : FUZZ);
@@ -509,21 +509,21 @@ jtccvt(J jt, I tflagged, A w, A *y) {
         case CVCASE(FLX, XNUMX): return convert<X, D>(jt, w, yv);
         case CVCASE(CMPXX, XNUMX):
             GATV(d, FL, n, r, s);
-            if (!(convert<X, D>(jt, w, AV(d)))) return 0;
+            if (!(convert<X, D>(jt, w, pointer_to_values(d)))) return 0;
             return convert<D, Z>(jt, d, yv);
         case CVCASE(B01X, RATX):
             GATV(d, XNUM, n, r, s);
-            if (!(convert<Q, X>(jt, w, AV(d)))) return 0;
+            if (!(convert<Q, X>(jt, w, pointer_to_values(d)))) return 0;
             return convert<X, bool>(jt, d, yv);
         case CVCASE(INTX, RATX):
             GATV(d, XNUM, n, r, s);
-            if (!(convert<Q, X>(jt, w, AV(d)))) return 0;
+            if (!(convert<Q, X>(jt, w, pointer_to_values(d)))) return 0;
             return convert<X, I>(jt, d, yv);
         case CVCASE(XNUMX, RATX): return convert<Q, X>(jt, w, yv);
         case CVCASE(FLX, RATX): return convert<Q, D>(jt, w, yv);
         case CVCASE(CMPXX, RATX):
             GATV(d, FL, n, r, s);
-            if (!(convert<Q, D>(jt, w, AV(d)))) return 0;
+            if (!(convert<Q, D>(jt, w, pointer_to_values(d)))) return 0;
             return convert<D, Z>(jt, d, yv);
         default: ASSERT(0, EVDOMAIN);
     }
@@ -557,16 +557,16 @@ jtbcvt(J jt, C mode, A w) {
     // convert the flagged values to float and keep the result as complex
     array result = w;
     if ((((AN(w) - 1) | (AT(w) & CMPX) - 1)) >= 0) {  // not empty AND complex
-        Z *wv      = ZAV(w);
+        Z *wv      = pointer_to_values<Z>(w);
         auto flags = std::transform_reduce(wv, wv + AN(w), int64_t{}, std::plus{}, isflag);
         if (flags) {
             I ipok = SGNIF(jtinplace, JTINPLACEWX) & AC(w);  // both sign bits set (<0) if inplaceable
             if (flags == AN(w)) {
                 if (ipok >= 0) GATV(result, INT, AN(w), AR(w), AS(w));
-                std::transform(wv, wv + AN(w), IAV(result), [&](auto const &z) { return as_integer(z.re); });
+                std::transform(wv, wv + AN(w), pointer_to_values(result), [&](auto const &z) { return as_integer(z.re); });
             } else {
                 if (ipok >= 0) GATV(result, CMPX, AN(w), AR(w), AS(w));
-                std::transform(wv, wv + AN(w), ZAV(result), [&](auto const &z) -> Z {
+                std::transform(wv, wv + AN(w), pointer_to_values<Z>(result), [&](auto const &z) -> Z {
                     if (isflag(z)) return {.re = (D)as_integer(z.re), .im = 0.0};
                     return z;  // copy floats, and converts any integers back to float
                 });
@@ -592,10 +592,10 @@ jtbcvt(J jt, C mode, A w) {
 A
 jticvt(J jt, A w) {
     auto const n = AN(w);
-    auto const* v = DAV(w);
+    auto const* v = pointer_to_values<double>(w);
     A z;
     GATV(z, INT, n, AR(w), AS(w));
-    auto u = AV(z);
+    auto u = pointer_to_values(z);
     for (int64_t i = 0; i < n; ++i) {
         auto x = *v++;
         if (x < IMIN || FLIMAX <= x) return w;  // if conversion will fail, skip it
@@ -619,7 +619,7 @@ jtcvt0(J jt, A w) {
     auto const t = AT(w);
     auto const n = (t & CMPX) ? 2 * AN(w) : AN(w);
     if (n && t & FL + CMPX) {
-        auto u = DAV(w);
+        auto u = pointer_to_values<double>(w);
         std::transform(u, u + n, u, [](auto v) { return v == 0.0 ? 0.0 : v; });
     }
     return w;
@@ -648,7 +648,7 @@ jtxco2(J jt, A a, A w) {
                 A z;
                 GATV(z, XNUM, 2 * n, r + 1, AS(w));
                 AS(z)[r] = 2;
-                memcpy(AV(z), AV(w), 2 * n * SZI);
+                memcpy(pointer_to_values(z), pointer_to_values(w), 2 * n * SZI);
                 return z;
             }
         default: ASSERT(0, EVDOMAIN);
