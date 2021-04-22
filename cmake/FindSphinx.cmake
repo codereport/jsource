b5a6db588235b56a2f17cc5caddb8c8223450ae9
Sebanisu <peeceseven@gmail.com>
2021-04-22 13:53:33 -0400

    Update FindSphinx.cmake
    added some code to make it better at finding sphinx. pip3 doesn't install things to the global bin folder. cmake is also checking for python3 and trying to use it.

--- cmake/FindSphinx.cmake
+++ cmake/FindSphinx.cmake
@@ -1,11 +1,22 @@
-#Look for an executable called sphinx-build
-find_program(SPHINX_EXECUTABLE
-             NAMES sphinx-build
-             DOC "Path to sphinx-build executable")
+find_package (Python3 COMPONENTS Interpreter Development REQUIRED)
+
+execute_process(
+        COMMAND ${Python3_EXECUTABLE}  "-c" "exec(\"import sphinx\\nimport os\\nprint (os.path.dirname(sphinx.__file__))\")"
+        RESULT_VARIABLE EXIT_CODE
+        OUTPUT_VARIABLE PIP3_SPHINX_DATA
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+message(STATUS ${Python3_EXECUTABLE})
+message(STATUS ${PIP3_SPHINX_DATA}/bin)
+find_program(SPHINX_EXECUTABLE NAMES sphinx-build
+        HINTS ${PIP3_SPHINX_DATA} ~/.local/bin
+        DOC "Sphinx documentation generator"
+        )
 
 include(FindPackageHandleStandardArgs)
 
-#Handle standard arguments to find_package like REQUIRED and QUIET
-find_package_handle_standard_args(Sphinx
-                                  "Failed to find sphinx-build executable"
-                                  SPHINX_EXECUTABLE)
+find_package_handle_standard_args(Sphinx DEFAULT_MSG
+        SPHINX_EXECUTABLE
+        )
+
+mark_as_advanced(SPHINX_EXECUTABLE)
